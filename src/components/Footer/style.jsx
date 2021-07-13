54e44f743e43ca602a3e1672b76c0a8adec1ce4f
Jakub Pawelski <j.pawelski@interia.pl>
2021-07-13 21:07:17 +0200

    create components folder,delete file and prepare  to work

---
+++ src/components/Footer/style.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  grid-area: footer;
+  display: flex;
+  background-color: #203040;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/components/Header/style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  grid-area: header;
+  padding: 0.5rem;
+  background-color: #203040;
+  color: #fff;
+  a {
+    color: #fff;
+  }
+`;
